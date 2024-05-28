4fb0874b46614b9506296e09cffe59313452a4e2
Petros <petros.chantz@gmail.com>
2024-05-28 21:06:49 +0200

    migrating code from old portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        "4k": "2000px",
+      },
+      colors: {
+        accent: "#0ea2e9",
+        accentHover: "#0287c7",
+        accentLightHover: "",
+        primary: "#202831",
+        greyDark: "#D7DCE0",
+        greyLight: "#fafafa",
+        greyHover: "#F5F7F9",
+      },
+    },
+  },
+  plugins: [],
+};
